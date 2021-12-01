078c1df57fde229bbf7432e5ffc89cce02fb9f90
Andrey Zadniprovskyi <macyalive@gmail.com>
2021-12-01 13:35:20 +0200

    First code (2 nd commit). Dot added to print

--- src/main/groovy/TaskOne.groovy
+++ src/main/groovy/TaskOne.groovy
@@ -1,2 +1,11 @@
 class TaskOne {
-}
+        static void main(String[] args) {
+            //Initializing 2 variables
+            int x = 5;
+            int X = 6;
+
+            //Printing the value of the variables to the console
+            println("The value of x is " + x +"." + "The value of X is " + X);
+        }
+    }
+
